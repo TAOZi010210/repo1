ac4b55b16b2f7f096b4a3a2d6d663a235320ec7f
TAOZi <2028200539@qq.com>
2022-09-20 20:23:16 +0800

    ok

---
+++ src/main/java/com/tao/HellolinuxApplication.java
@@ -0,0 +1,13 @@
+package com.tao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellolinuxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellolinuxApplication.class, args);
+    }
+
+}
